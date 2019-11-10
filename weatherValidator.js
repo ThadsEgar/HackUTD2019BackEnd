ecebf841b0feb5324879613d101b56c8c759cef0
abhikamboji <abhi.kamboji@gmail.com>
2019-11-09 22:40:39 -0600

    base code for open weather

---
+++ weatherValidator.js
@@ -0,0 +1,7 @@
+const AccuWeatherWrapper = require('accuweather-wrapper').default
+ 
+// passing access token via options
+const accuweather = new AccuWeatherWrapper({
+  token: 'UzNmuQwExG75GdW94w77AG0s5ixUZmGU'
+})
+ 
